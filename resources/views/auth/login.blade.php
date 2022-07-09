f613387a31c57c0d7fcae9ec63aee7ad409b093e
klismaAlbornoz <albornozcajasklisman@gmail.com>
2022-07-08 22:10:06 -0500

    casi finalo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-9 d-flex justify-content-center">
+            <img src="{{asset("fotoss/banerr.jpg")}}">
+        </div>
+        <div class="col-md-3">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
@@ -11,11 +14,13 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="row mb-1">
+                            <label for="email" class="col-md-5 col-form-label text-md-end">ingrese email</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="col-md-7">
+                                <input id="email" type="email"
+                                       class="form-control @error('email') is-invalid @enderror"
+                                       name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -25,10 +30,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="row mb-1">
+                            <label for="password" class="col-md-5 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -53,7 +58,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-secondary">
                                     {{ __('Login') }}
                                 </button>
 
@@ -67,7 +72,56 @@
                     </form>
                 </div>
             </div>
+
         </div>
     </div>
+</div>
+<div class="bg-white mb-3">
+    <div class="container">
+        <div class="row">
+            <div class="col mb-3">
+                <div class="d-flex justify-content-center  mb-3">
+                    <img src="{{asset("fotoss/logoudh3.jpg")}}">
+
+                </div>
+                <p>
+                <b>OFICINA CENTRAL</b><br>
+                Jr. Hermilio Valdizán # 871 - Huánuco
+                </p>
+                <p>
+                <b>Teléfono: (51-62) 519773</b><br>
+                </p>
+                <p>
+                    <b>CIUDAD UNIVERSITARIA</b><br>
+                Carretera Central km 2.6 - Huánuco
+                </p>
+                <p>
+                <b>Teléfono: (51-62) 515151</b><br>
+                </p>
+
+            </div>
+            <div class="col">
+                <h1> paginas </h1><br>
+                <div class="d-flex justify-content-center  mb-3">
+                    <img style="border: 1px solid #cccccc" src="{{asset("fotoss/sistemas.jpg")}}">
+
+                </div>
+            </div>
+            <div class="col mt-3">
+                <h2>Enlaces </h2>
+                <ul class="text-success text-decoration-underline">
+                    <li>Preguntas Frecuentes</li>
+                    <li>Oficina de admisión</li>
+                    <li>Programa de Educación Superior a Distancia</li>
+                    <li>Vicerrectorado de investigación</li>
+                    <li>Ubicación y contactos</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="bg-success" style="height: 10px"></div>
+
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,11 +20,12 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+<div class="bg-success" style="height: 10px" ></div>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand mb-2" href="{{ url('/') }}">
+                    <img src="{{asset("fotoss/logoudh3.jpg")}}">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -53,17 +54,17 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
+                          {{--  @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
-                            @endif
+                            @endif--}}
 
-                            @if (Route::has('register'))
+                            {{--@if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif--}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
